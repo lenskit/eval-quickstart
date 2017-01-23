df57c3a82bbb7f4847b593f7778bdfa9f6a6bf31
dkluver <dkluver@macalester.edu>
2017-01-23 16:59:34 -0600

    updated code base - download url was failing and some code had old org.grouplens imports

--- algorithms/item-item.groovy
+++ algorithms/item-item.groovy
@@ -1,8 +1,8 @@
 import org.lenskit.baseline.BaselineScorer
 import org.lenskit.baseline.ItemMeanRatingItemScorer
 import org.lenskit.knn.item.ItemItemScorer
-import org.grouplens.lenskit.transform.normalize.BaselineSubtractingUserVectorNormalizer
-import org.grouplens.lenskit.transform.normalize.UserVectorNormalizer
+import org.lenskit.transform.normalize.BaselineSubtractingUserVectorNormalizer
+import org.lenskit.transform.normalize.UserVectorNormalizer
 
 bind ItemScorer to ItemItemScorer
 bind UserVectorNormalizer to BaselineSubtractingUserVectorNormalizer
